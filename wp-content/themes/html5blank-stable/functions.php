ead946493877b53fc973e345110feea566f1e89d
oromp <nguyenvinhyphat@yahoo.com.vn>
2017-03-31 18:27:41 +0700

    fix nav bar

--- wp-content/themes/html5blank-stable/functions.php
+++ wp-content/themes/html5blank-stable/functions.php
@@ -486,5 +486,5 @@ function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 short
 {
     return '<h2>' . $content . '</h2>';
 }
-require_once('wp_bootstrap_navwalker.php');
+require_once('wp-bootstrap-navwalker.php');
 ?>
--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -2,23 +2,30 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+    <title><?php wp_title(''); ?><?php if (wp_title('', false)) {
+            echo ' :';
+        } ?><?php bloginfo('name'); ?></title>
 
     <link href="//www.google-analytics.com" rel="dns-prefetch">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-    <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet" >
+    <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+          rel="stylesheet">
     <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="keywords" content="Blog Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android
     Compatible web template,
-    Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+    Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design"/>
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-    function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <script type="application/x-javascript"> addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        } </script>
     <?php wp_head(); ?>
 
     <script>
@@ -31,39 +38,39 @@
     </script>
     <!-- main slider banner -->
     <script type="text/javascript">
-        jQuery(document).ready(function($){
-            jQuery('#demo1').skdslider({'delay':5000, 'animationSpeed': 2000,'showNextPrev':true,'showPlayButton':true,'autoSlide':true,'animationType':'fading'});
+        jQuery(document).ready(function ($) {
+            jQuery('#demo1').skdslider({
+                'delay': 5000,
+                'animationSpeed': 2000,
+                'showNextPrev': true,
+                'showPlayButton': true,
+                'autoSlide': true,
+                'animationType': 'fading'
+            });
 
-            jQuery('#responsive').change(function(){
+            jQuery('#responsive').change(function () {
                 $('#responsive_wrapper').width(jQuery(this).val());
             });
 
         });
     </script>
-
-
-
-    <!-- start smooth scrolling-->
     <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            $(".scroll").click(function(event){
+        jQuery(document).ready(function ($) {
+            $(".scroll").click(function (event) {
                 event.preventDefault();
-                $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+                $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
             });
         });
     </script>
-
-
-    <!-- smooth drop down nav -->
     <script>
-        jQuery(document).ready(function($){
+        jQuery(document).ready(function ($) {
             $(".dropdown").hover(
-                function() {
-                    $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+                function () {
+                    $('.dropdown-menu', this).stop(true, true).slideDown('fast');
                     $(this).toggleClass('open');
                 },
-                function() {
-                    $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+                function () {
+                    $('.dropdown-menu', this).stop(true, true).slideUp('fast');
                     $(this).toggleClass('open');
                 }
             );
@@ -76,7 +83,7 @@
 <div class="wrapper">
 
     <!-- header -->
-    <header >
+    <header>
         <div class="w3layouts-top-strip">
             <div class="container">
                 <div class="logo">
@@ -101,55 +108,46 @@
 
         <!-- nav -->
         <nav class="navbar navbar-default">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-
-
-
-
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a class="active" href="index.html">Home</a></li>
-                    <?php html5blank_nav(); ?>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Short Codes <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="icons.html">Icons Page</a></li>
-                            <li><a href="typo.html">Typography</a></li>
-
-                        </ul>
-                    </li>
-                    <li><a href="photography.html">Photography</a></li>
-                    <li><a href="features.html">Features</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-            <div class="w3_agile_login">
-                <div class="cd-main-header">
-                    <a class="cd-search-trigger" href="#cd-search"> <span></span></a>
-                    <!-- cd-header-buttons -->
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                 </div>
-                <div id="cd-search" class="cd-search">
-                    <form action="#" method="post">
-                        <input name="Search" type="search" placeholder="Search...">
-                    </form>
-                </div>
-            </div>
-            <div class="clearfix"> </div>
 
-        </div><!-- /.container-fluid -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <?php /* Primary navigation */
+                    wp_nav_menu(array(
+                            'theme_location' => 'header-menu',
+                            'depth' => 2,
+                            'container' => 'div',
+                            'container_class' => 'collapse navbar-collapse',
+                            'container_id' => 'bs-example-navbar-collapse-1',
+                            'menu_class' => 'nav navbar-nav',
+                            'walker' => new wp_bootstrap_navwalker())
+                    );
+                    ?><!-- /.navbar-collapse -->
+                </div>
+                <div class="w3_agile_login">
+                    <div class="cd-main-header">
+                        <a class="cd-search-trigger" href="#cd-search"> <span></span></a>
+                        <!-- cd-header-buttons -->
+                    </div>
+                    <div id="cd-search" class="cd-search">
+                        <form action="#" method="post">
+                            <input name="Search" type="search" placeholder="Search...">
+                        </form>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+
+            </div><!-- /.container-fluid -->
         </nav>
 
 
@@ -160,7 +158,6 @@
 
         </div>
         <!-- main-slider-->
-
 
 
     </header>
